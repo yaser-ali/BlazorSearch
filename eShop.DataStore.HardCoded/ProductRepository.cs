700914b959ce2eff875bfab0bc42e892a9c31b55
Yaser Ali <yaser.ali32@hotmail.co.uk>
2020-10-21 20:59:40 +0100

    Update ProductRepository.cs

--- eShop.DataStore.HardCoded/ProductRepository.cs
+++ eShop.DataStore.HardCoded/ProductRepository.cs
@@ -14,7 +14,9 @@ namespace eShop.DataStore.HardCoded
         {
             products = new List<Product>
             {
-                new Product { Id = 2, Type = "Streamshare", Name = " Premium HD Video Streamshare", Price= 5.99, Description= "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more – on thousands of internet-connected devices.You can watch as much as you want, whenever you want, without a single advert – all for one low monthly price. There’s always something new to discover, and new TV programmes and films are added every week!", ImageLink="https://go5050.co.uk/wp-content/uploads/2020/07/gfffavicon-min.png"}
+                new Product { Id = 2, Type = "Streamshare", Name = "Premium HD Video Streamshare", Price= 5.99, Description= "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more – on thousands of internet-connected devices.You can watch as much as you want, whenever you want, without a single advert – all for one low monthly price. There’s always something new to discover, and new TV programmes and films are added every week!", ImageLink="https://go5050.co.uk/wp-content/uploads/2020/07/gfffavicon-min.png"},
+                new Product { Id = 3, Type = "Streamshare", Name = "Sport Pass Cinema Pass", Price= 5.99, Description= "Watch the latest movies, tv shows, live sports and kids entertainment live and on demand.", ImageLink="https://go5050.co.uk/wp-content/uploads/2020/08/ces-prod.png"}
+
             };
         }
 
