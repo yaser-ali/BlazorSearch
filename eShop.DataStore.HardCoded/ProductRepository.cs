1d2a190d92a7b37cc74f2d6996b93bc8c7d30bb6
Yaser <yaser.ali32@hotmail.co.uk>
2020-10-21 20:49:36 +0100

    Adding fies to repo

---
+++ eShop.DataStore.HardCoded/ProductRepository.cs
@@ -0,0 +1,33 @@
+﻿using eShop.CoreBusiness.Models;
+using eShop.UseCases.PluginInterfaces.DataStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace eShop.DataStore.HardCoded
+{
+    public class ProductRepository : IProductRepository
+    {
+        private List<Product> products;
+
+        public ProductRepository()
+        {
+            products = new List<Product>
+            {
+                new Product { Id = 2, Type = "Streamshare", Name = " Premium HD Video Streamshare", Price= 5.99, Description= "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more – on thousands of internet-connected devices.You can watch as much as you want, whenever you want, without a single advert – all for one low monthly price. There’s always something new to discover, and new TV programmes and films are added every week!", ImageLink="https://go5050.co.uk/wp-content/uploads/2020/07/gfffavicon-min.png"}
+            };
+        }
+
+        public Product GetProduct(int id)
+        {
+            return products.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Product> GetProducts(string filter = null)
+        {
+            if (string.IsNullOrWhiteSpace(filter)) return products;
+
+            return products.Where(x => x.Name.ToLower().Contains(filter.ToLower()));
+        }
+    }
+}
